301bf2acd5f7794d98b59f2643f506cfa750b11e
DeviousLab <deviouslab@gmail.com>
2023-08-08 14:49:26 +0400

    feat: removed firebase auth and implemented clerk
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- apps/app/src/pages/account/register.tsx
+++ apps/app/src/pages/account/register.tsx
@@ -1,24 +1,12 @@
 import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
 
 import Footer from '../../components/account/Footer';
 import Header from '../../components/account/Header';
 import RegisterForm from '../../components/account/RegisterForm';
-import { auth } from '../../firebase/client';
 import { Meta } from '../../layouts/Meta';
 
 const Register: NextPage = () => {
-  const router = useRouter();
-  const [user, loadingUser] = useAuthState(auth);
 
-  useEffect(() => {
-    if(user) {
-      router.push('/account/verify-email');
-    }
-  }, [user]);
-  
   return (
     <>
       <Meta title="triage | Register" description="Register for an account" />
