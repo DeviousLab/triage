3426ee204c2d38c18ea3a4700b3a003b9d1455d8
DeviousLab <deviouslab@gmail.com>
2023-08-08 14:49:43 +0400

    fix: added homepage link to header
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- apps/app/src/components/account/Header.tsx
+++ apps/app/src/components/account/Header.tsx
@@ -1,21 +1,25 @@
+import Link from 'next/link';
+
 import Logo from '../../assets/images/temp-logo.svg';
 
 const Header = () => {
-  return (
-    <header>
-      <div className="container mx-auto flex flex-col flex-wrap items-center p-5">
-        <a className="mb-4 flex items-center font-medium text-gray-900 md:mb-0 lg:justify-center">
-          <img
-            className="w-8 cursor-pointer"
-            src={Logo.src}
-            alt="triage logo"
-          />
-          <span className="ml-2 font-Poppins text-xl font-bold text-logo">
-            triage
-          </span>
-        </a>
-      </div>
-    </header>
-  );
+	return (
+		<header>
+			<div className='container mx-auto flex flex-col flex-wrap items-center p-5'>
+				<Link href={'/'}>
+					<a className='mb-4 flex items-center font-medium text-gray-900 md:mb-0 lg:justify-center'>
+						<img
+							className='w-8 cursor-pointer'
+							src={Logo.src}
+							alt='triage logo'
+						/>
+						<span className='ml-2 font-Poppins text-xl font-bold text-logo'>
+							triage
+						</span>
+					</a>
+				</Link>
+			</div>
+		</header>
+	);
 };
 export default Header;
