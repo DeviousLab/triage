63515eaa6204b38392e108fe19fc9bca0ebce84d
DeviousLab <deviouslab@gmail.com>
2023-08-15 19:26:19 +0400

    feat: implemented under construction page and temp middleware
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- apps/app/src/components/account/Header.tsx
+++ apps/app/src/components/account/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
 	return (
 		<header>
 			<div className='container mx-auto flex flex-col flex-wrap items-center p-5'>
-				<Link href={'/'}>
+				<Link href={'https://mytriage-app.vercel.app/'}>
 					<a className='mb-4 flex items-center font-medium text-gray-900 md:mb-0 lg:justify-center'>
 						<img
 							className='w-8 cursor-pointer'
--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -28,6 +28,19 @@ module.exports = {
         Poppins: ['Poppins', 'sans-serif'],
         Arimo: ['Arimo', 'sans-serif'],
       },
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 5s ease infinite alternate',
+      },
     },
   },
   // eslint-disable-next-line global-require
